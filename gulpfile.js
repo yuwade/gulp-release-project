cbcaf97c3de07cfba6bad21bc36909b2f2463776
yuwade <446138762@qq,cin>
2015-12-02 21:47:20 +0800

    添加 readme

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var requirejsOptimize = require('gulp-requirejs-optimize');
 var uglify = require('gulp-uglify');
 gulp.task('default',['scripts'], function () {
     // 将你的默认的任务代码放在这
-    console.log("123")
 });
 gulp.task('scripts', function () {
     return gulp.src('D:/WorkPlace/htmls/requirejs/app.js')
---
+++ readme.md
@@ -0,0 +1,3 @@
+发布项目
+    项目中使用requirejs，把项目合并一个文件并混淆
+    修改 task scripts路径
